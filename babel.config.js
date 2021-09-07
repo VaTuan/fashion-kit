c50f1c519eb4117e8e13caf8d5029988c85d1b62
tuanva <tuanva@vmodev.com>
2021-09-07 15:11:32 +0700

    update boilerpalte

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,27 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+    presets: ["module:metro-react-native-babel-preset"],
+    plugins: [
+        [
+            "module-resolver",
+            {
+                root: ["./src"],
+                extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
+                alias: {
+                    tests: ["./tests/"],
+                    "@components": "./src/components",
+                    "@authentication": "./src/Authentication",
+                    "@assets": "./src/assets",
+                    "@navigations": "./src/navigations",
+                    "@screens": "./src/screens",
+                    "@services": "./src/services",
+                    "@styles": "./src/styles",
+                    "@types": "./src/types",
+                    "@utils": "./src/utils",
+                    "@store": "./src/store",
+                    "@hooks": "./src/hooks",
+                    "@theme": "./src/theme",
+                },
+            },
+        ],
+    ],
 };
---
+++ src/types/stack.navigator.ts
@@ -0,0 +1,5 @@
+export type RootStackParamList = {
+    Onboarding: undefined;
+};
+
+export type AuthenticationStackParamList = Pick<RootStackParamList, "Onboarding">;
